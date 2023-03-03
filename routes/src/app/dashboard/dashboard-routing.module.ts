44131ce46f0537bd808434d004d900402c14bdab
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-03-03 15:53:41 -0300

    Rotas

--- routes/src/app/dashboard/dashboard-routing.module.ts
+++ routes/src/app/dashboard/dashboard-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 //Dashboard
 import { HomeComponent } from './pages/home/home.component';
 
-
 const routes: Routes = [
 {
   path: '', component:HomeComponent
