424c3682f939efbb6892ef6bc264a5bba3c06e0b
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-03-03 15:28:06 -0300

    Dashboard

--- routes/src/app/app-routing.module.ts
+++ routes/src/app/app-routing.module.ts
@@ -3,15 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 //Pages
 import { HomeComponent } from './pages/home/home.component';
-import { PageErrorComponent } from './pages/page-error/page-error.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
 
 const routes: Routes = [
   { path:'', component: HomeComponent, pathMatch:'full'},
-
-  { path:'sobre', component: SobreComponent, children: [ //    :id/:username
-    { path:'daniel', component: SobreComponent}
-   ]}, //    :id/:username
+  {
+    path:'sobre', component: SobreComponent, children: [
+     { path:'daniel', component: SobreComponent}
+    ]
+  },
+  {
+    path: 'dashboard',
+    loadChildren:() => import('./dashboard/dashboard.module').then( m => m.DashboardModule)
+  },
   { path:'404', component: PageErrorComponent},
   { path:'**', redirectTo: '404'}
 ];
