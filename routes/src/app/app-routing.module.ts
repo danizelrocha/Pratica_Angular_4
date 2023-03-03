b1565570c808d2a1b1f406e5ee6f6649fe7db22a
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-03-02 22:50:16 -0300

    Abertura de Rotas

--- routes/src/app/app-routing.module.ts
+++ routes/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Pages
+import { HomeComponent } from './pages/home/home.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+
+const routes: Routes = [
+  { path:'', component: HomeComponent, pathMatch:'full'},
+  { path:'404', component: PageErrorComponent},
+  { path:'sobre', component: SobreComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
