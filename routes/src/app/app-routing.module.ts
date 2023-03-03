b974f583ff2958619ff840ca3d502e44cd134816
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-03-02 22:54:12 -0300

    Rota Coringa

--- routes/src/app/app-routing.module.ts
+++ routes/src/app/app-routing.module.ts
@@ -9,7 +9,8 @@ import { SobreComponent } from './pages/sobre/sobre.component';
 const routes: Routes = [
   { path:'', component: HomeComponent, pathMatch:'full'},
   { path:'404', component: PageErrorComponent},
-  { path:'sobre', component: SobreComponent}
+  { path:'sobre', component: SobreComponent},
+  { path:'**', redirectTo: '404'}
 ];
 
 @NgModule({
